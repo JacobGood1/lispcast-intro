eb2d854b388e2c977b61cbe176d2e461a7cf2ae2
jacob <submissionfighting@gmx.com>
2014-08-26 16:38:58 -0400

    small change

--- src/lispcast_intro/core.clj
+++ src/lispcast_intro/core.clj
@@ -13,9 +13,28 @@
 (defn about
   [req]
   {:status 200 :body "hi my name is Jacob this site will be created in due time!" :headers {}})
+(defn request
+  [req]
+  (println req))
+(defn yo-name
+  [req]
+  (println req)
+  {:status 200 :body (str "yo " (:list-id (:route-params req))) :headers {}})
+
+(defn calc-it
+  [{{:keys [first-num op next-num]} :route-params}]
+  {:status 200
+   :body (str (reduce (eval (symbol op))
+                      (map #(Integer/parseInt %) [first-num next-num])))
+   :headers {}})
+
 (defroutes app
            (GET "/" [] greet)
+           (GET "/calc/:first-num/:op/:next-num" [] calc-it)
+           (GET "/lists/:list-id/edit" [] yo-name)
            (GET "/goodbye" [] goodbye)
+           (GET "/about" [] about)
+           (GET "/request" [] request)
            (not-found "Page not found!"))
 
 (defn -main [port]
